b4e61bfb8b88bfd58714a7794041f3393972b4ba
Marcus Merrick <marcusmerrick@hotmail.com>
2020-06-19 17:55:14 -0400

    'in process...'

---
+++ main.js
@@ -0,0 +1,36 @@
+//office.png, under will be "if you could click me, that would be greeeaaat" link: https://en.wikipedia.org/wiki/Office_Space
+//button makes image/link appear/disappear
+
+let body = document.querySelector('body')
+
+let main = document.createElement('main')
+let btn = document.createElement('button')
+
+body.prepend(main)
+body.prepend(btn)
+
+
+// let IMG = document.body.main.createElement('img')
+// let office = new Image()
+// office.onload = function() {
+//    IMG.src = "./office.png"
+// }
+
+let src = document.createElement('head')
+
+
+//class image
+
+
+
+//class link
+
+
+//button
+
+
+//image
+// let office2 = document.createElement("div")
+// office2.append.src = "./office.png";
+
+
